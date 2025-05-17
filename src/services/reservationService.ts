4cfdfb79cf4d82f07e0f2321c3722f262f7e166e
Jean Grandmaire <grandmaire@gmail.com>
2025-05-16 21:04:30 -0700

    Successful test, ready for Sat

--- src/services/reservationService.ts
+++ src/services/reservationService.ts
@@ -9,13 +9,13 @@ export class ReservationService {
         const morningButton = await this.page.$(mbSelector);
         // check that morningButton is not null
         if (morningButton === null) {
-            console.log('Morning button is not available, clicking on the next date then come back later');
+            console.log(`${timeOfDay} button is not available, clicking on the next date then come back later`);
         } else {
             console.log('Clicking on the morning button');
             // log the text content of the morning button
             const morningText = await morningButton?.evaluate((el) => el.textContent);
             // log the morning button text
-            console.log('Morning button text:', morningText);
+            console.log('Time of day button text:', morningText);
             await morningButton.click();
             //wait for next UI change
             await this.page.waitForSelector('div.times-inner.time-tiles.js-times', { visible: true });
@@ -31,11 +31,12 @@ export class ReservationService {
     async clickNumberOfPlayers(numberOfPlayers: number = 4) {
         //log the number of players
         console.log('Number of players:', numberOfPlayers);
+        
         // create constants for the number of players buttons selector
         const numberOfPlayersSelector = `a.btn.btn-primary[data-value="${numberOfPlayers}"]:not(.disabled)`;
+        await this.page.waitForSelector(numberOfPlayersSelector, { visible: true });
         const numberOfPlayersButton = await this.page.$(numberOfPlayersSelector);
         // check that numberOfPlayersButton is not null
-        await this.page.waitForSelector(numberOfPlayersSelector, { visible: true });
 
         if (numberOfPlayersButton === null) {
             console.log('Number of players button is not available, clicking on the next date then come back later');
@@ -56,8 +57,9 @@ export class ReservationService {
 
     async clickTimeSlot(): Promise<boolean> {
         // Wait for the time slots to be visible
+        await new Promise(res => setTimeout(res, 50));
         await this.page.waitForSelector('div.times-inner.time-tiles.js-times', { visible: true });
-
+        
         // create a variable named timeSlots with an array comprised of all the children of <div class="times-inner  time-tiles  js-times">
         const timeSlots = await this.page.$$('div.times-inner.time-tiles.js-times > div');
 
@@ -88,7 +90,7 @@ export class ReservationService {
             //     return false;
             // }
             // Check if the text content matches the slotId
-            if (timeSlotText?.trim().startsWith("10:") || timeSlotText?.trim().startsWith("11:")) {
+            if (timeSlotText?.trim().startsWith("4:") || timeSlotText?.trim().startsWith("5:")) {
                 // Click on the time slot
                 await timeSlot.click();
                 // wait for response to POST request https://foreupsoftware.com/index.php/api/booking/pending_reservation
@@ -116,11 +118,13 @@ export class ReservationService {
                 return response.url().includes('https://foreupsoftware.com/index.php/api/booking/times') && response.status() === 200;
             });
             // Click on the number of players button
-            await this.clickNumberOfPlayers();
+            // await this.clickNumberOfPlayers();
+
+            await this.clickTimeOfDay("morning");
 
             await this.page.waitForSelector('div.times-inner.time-tiles.js-times', { visible: true });
             const timeH = "19"
-            const timeM = "02"
+            const timeM = "00"
             // Schedule a task to run at certain time,  asychronously. Select time that starts with 4:
             console.log(`Schedule reservation today at ${timeH}:${timeM}`);
 
@@ -128,16 +132,15 @@ export class ReservationService {
                 var nothingReserved = true;
                 var numberOfPlayers = 4;
                 console.log(`Trying to find a time slot for ${numberOfPlayers} players`);
-
+                await this.clickNumberOfPlayers(numberOfPlayers);
                 while (nothingReserved && (numberOfPlayers > 1)) {
-                    this.clickTimeOfDay();
                     // if click time slot returns false, try again with one less player
                     if (await this.clickTimeSlot() === false) {
                         console.log('No time slot found, trying again');
                         numberOfPlayers--;
-                        // wait for 100ms
+                        // wait for 50ms
                         await new Promise(res => setTimeout(res, 50));
-                        await this.clickNumberOfPlayers(numberOfPlayers);
+                        await this.clickNumberOfPlayers(numberOfPlayers);   
                     } else {
                         //log message "Reservation successful, make manual reservation"
                         console.log('Reservation successful, make manual reservation');
